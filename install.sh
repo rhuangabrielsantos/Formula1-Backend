3c8dbba94e0b8636fd87a5b5b9226e858d40f777
Rhuan Santos <rhuangs_@hotmail.com>
2019-06-25 14:53:14 -0300

    Project TG Implementando instalação pelo arquivo install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
-sudo usermod -aG docker ${USER}
-su - ${USER}
-
-docker-compose up -d
-
 docker run --rm --interactive --tty \
   --volume $PWD:/app \
   --user $(id -u):$(id -g) \
